1ba676e7082fab4c63deb9b6cc4f3f3de35cb95a
Amisi Kiarie <8081220+akiarie@users.noreply.github.com>
2021-12-18 17:39:56 +0300

    progress

--- quines/python/ipse.py
+++ quines/python/ipse.py
@@ -1,5 +1,9 @@
-rho = b'\n# contains everything in the program from the newline before this line\n\nprint(f"rho = {rho}", end="")\nprint(rho.decode(\'utf-8\'))'
-# contains everything in the program from the newline before this line
+rho = """
 
-print(f"rho = {rho}", end="")
-print(rho.decode('utf-8'))
+# sigma:
+print(f"rho = {rho}", end="") # print(rho)
+print(rho.decode("utf-8"))    # print(R^-1(rho))""".encode("utf-8")
+
+# sigma:
+print(f"rho = {rho}", end="") # print(rho)
+print(rho.decode("utf-8"))    # print(R^-1(rho))
